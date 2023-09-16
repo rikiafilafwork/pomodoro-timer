e3c0fb45608fb543d0cf38802f2e6f590f741dde
Riki Afila F <rikiafilaf.work@gmail.com>
2023-09-16 08:20:32 +0700

    Update app, tambah komen

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-// const notifier = require("node-notifier");
-// const moment = require("moment");
 import notifier from 'node-notifier'
 import moment from 'moment'
 
+//Mengambil data argument node
+//arg di slice array karena index 0 adalah path node, dan index 1 adalah path js yang di run
+//disini kita hanya membutuhkan argv yang kita set
 const argTime = process.argv.slice(2);
 
+//waktu kerja, jika tidak set argv, maka otomatis 25 menit
 const POMODORO_DURATION = argTime[0] ?? 25;
+//waktu istirahat, jika tidak set argv, maka otomatis 5 menit
 const BREAK_DURATION = argTime[1] ?? 5;
 
 let isWorking = false;
